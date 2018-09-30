273aa62dc762dc9c659a9c8a4fce444d0f45640a
Omar A <oazzat19@gmail.com>
2018-09-30 00:31:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,13 @@ def long_planeteer_calls(array)
   return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |item|
+    cheese_types.each do |cheese|
+      if cheese == item
+        return item
+    end
+  end
+  return false
 end
