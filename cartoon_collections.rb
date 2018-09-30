5d39d96abe61c4d0d454008263e72beaf0110ee0
Omar A <oazzat19@gmail.com>
2018-09-30 00:24:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (names)
+  counter = 1 
+  names.each do |name|
+    puts "#{counter} #{name}"
+    counter +=1 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |item|
+    item = item.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls()
+  
 end
 
 def find_the_cheese# code an argument here
